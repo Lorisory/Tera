76a6aa29a4082ffc6a634783dccd308c55e98e2e
Lorisory <133034944+Lorisory@users.noreply.github.com>
2025-04-07 07:03:37 +0200

    Init of Git

---
+++ Tera/Core/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Prism.Ioc;
+using Core.Views;
+using System.Windows;
+
+namespace Core
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
